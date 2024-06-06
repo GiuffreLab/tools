ec43b09166d93c630f1cc312b396d9da6449a851
Brian Giuffre <69125193+GiuffreLab@users.noreply.github.com>
2024-06-06 13:35:15 -0400

    Update lens.py

--- lens.py
+++ lens.py
@@ -3,37 +3,9 @@
 # This Python script calculates and displays the network address,
 # broadcast address, and usable host range for a given IP address
 # and either a subnet mask or CIDR notation.
-#
-# Users will be prompted to enter an IP address and either a subnet mask or CIDR.
-# The script will then compute the network and broadcast addresses, along with the range of usable hosts.
-# Finally, it will print these details to the console, including the total number of usable hosts.
 
-# How to use this script:
-
-# from network_info import NetworkInfo
-
-# def some_function():
-#     ip = "192.168.1.1"
-#     subnet = "255.255.255.0"
-
-#     network_info = NetworkInfo()
-#     network = network_info.get_network_address(ip, subnet, type)
-#     broadcast = network_info.get_broadcast_address(ip, subnet, type)
-#     usable = network_info.get_usable_host_range(ip, subnet, type)
-#     network_info.print_results(network, broadcast, usable)
-
-# some_function()
-
-# The above code will print the following to the console:
-# The network address is:
-# The broadcast address is:
-# Total usable hosts:
-# The usable host range is:
-
-
-
-# Script begins now
 from colorama import Fore, Style, init
+import os
 import ipaddress
 
 class NetworkInfo:
@@ -66,10 +38,25 @@ class NetworkInfo:
         else:
             print(Fore.GREEN + "The usable host range is: " + Style.RESET_ALL + f"{usable[0]} - {usable[-1]}")
 
+def check_root():
+    return os.geteuid() == 0
+
+def welcome_message():
+    user_name = os.getlogin()
+    print("-------------------------------------------------")
+    print("Welcome to Lens!")
+    print(f"Run by \033[35m{user_name}\033[0m")
+    if check_root():
+        print("Lens is being run \033[31mwith\033[0m Root privileges!")
+    else:
+        print("Lens is being run \033[32mwithout\033[0m Root privileges!")
+    print("-------------------------------------------------")
 
 # The following block will only execute when the script is run standalone, not when imported
 if __name__ == '__main__':
     init()
+
+    welcome_message()
 
     ip = input(Fore.CYAN + "Please enter the IP address: " + Style.RESET_ALL)
     subnet_or_cidr = input(Fore.CYAN + "Please enter the subnet mask or CIDR: " + Style.RESET_ALL)
